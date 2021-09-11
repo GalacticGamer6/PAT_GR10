41fee0f9d4336a48194d0c0bfbf1dc5ffeb12088
Neeraavr <Neeraavr@ZAKZRHB-CLLB22D.ballito.local>
2021-09-11 14:59:05 +0200

    Fixed timer and linked scramble algorithm

---
+++ src/main/java/Backend/Alg_trainer_Stuff.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend;
+
+/**
+ *
+ * @author Neeraavr
+ */
+public class Alg_trainer_Stuff {
+    
+    public static String [] AntiSune = {"U2 B D F2 D' B R2 D2 F2 U' D2 R2 U R2 B2 U' L2", "U' R' D' L D R' D' L' U F2 U F2 U' F2 U F2 R2 D B2 U' B2" , "U2 L U2 L' U' L U' L' U' L2 F2 B2 U F2 U' F2 D F2 D' B2 L2" };
+    public static String [] Sune = {"U2 L' U' L U' L' U2 L U' L2 B2 F2 U F2 U' F2 D F2 D' B2 L2" , "U' B D F2 D' B U2 B2 U R2 U2 B2 U' L2 B2 U' R2" , "U' L' U' L U' L' U2 L U' L2 B2 F2 U F2 U' F2 D F2 D' B2 L2" };
+    public static String [] L = {"L D R2 D' L D2 B2 D' R2 D' L2 U2 F2 U F2 U' F2" , "U' F2 U R U' L2 U R B2 U L2 U' F2 D L2 D R2 B2 L2" , "F L2 F' R2 F L2 F R2 B2 U L2 D L2 U' B2 U F2 R2 F2"};
+    public static String [] U = {"U' F' U' L2 F L2 F2 U F L2 R2 U F2 U' R2 B2 D L2 B2" , "U' B U' L2 U L2 U B R2 F2 U' R2 D R2 U F2 D2 R2 B2" , "L' D' R2 D L U L2 U2 F2 U F2 D' L2 R2 B2 U B2 D", };
+    public static String [] H = {"U2 F' U2 F U' L2 F D F D2 F2 R2 U' B2 D B2 D' R2 U L2" , "U F D R2 D R2 B2 D' F R2 U F2 U D B2 U' D2 L2" , "U B' U2 B U' R2 B D B' R2 U B2 D' F2 L2 D' F2 U B2 R2 D"};
+    public static String [] Pi = {"U' B U2 B2 R2 B' R2 U2 B U B2 U B2 U' B2 R2 B2 R2", "U L F2 L' U F2 L' U' L F2 R2 F2 U' F2 U' R2 U R2 U R2", "U2 R U2 R' U F2 L' U' L B2 U B2 L2 D L2 D' R2 D R2 F2" };
+    public static String [] T = {"U' R D L2 D' R U' D F2 R2 D' R2 U F2 D' L2 R2", "B U B D B' U' B L2 U' F2 U F2 D' L2 B2", "U2 L U L D L' U' L U2 L2 B2 U F2 D F2 D2 R2 D' R2 D B2"};
+    
+}
